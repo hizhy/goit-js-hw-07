e796417b739c4172fa69bac96579c79c1a2505e5
Mykola_Kh <hizhym@gmail.com>
2022-10-12 02:58:25 +0300

    2nd gallery finished

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,30 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const imgGalleryRef = document.querySelector(".gallery");
+const galleryMarkUp = createGalleryMarkup(galleryItems);
+imgGalleryRef.innerHTML = galleryMarkUp;
 
-console.log(galleryItems);
+imgGalleryRef.addEventListener("click", onImageClick);
+
+function createGalleryMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+      <a class="gallery__item" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+      </a>
+      `;
+    })
+    .join("");
+}
+
+function onImageClick(event) {
+  event.preventDefault();
+}
+
+let lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
+
+// console.log(galleryItems);
