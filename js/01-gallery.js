264c85e33eede5de21008005c3f4b4e604314081
Mykola_Kh <hizhym@gmail.com>
2022-10-12 01:17:00 +0300

    1st gallery finished

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,43 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const imgGalleryRef = document.querySelector(".gallery");
+const galleryMarkUp = createGalleryMarkup(galleryItems);
+imgGalleryRef.innerHTML = galleryMarkUp;
 
-console.log(galleryItems);
+imgGalleryRef.addEventListener("click", onImageClick);
+
+function createGalleryMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+        <div class="gallery__item">
+          <a class="gallery__link" href="${original}">
+            <img
+              class="gallery__image"
+              src="${preview}"
+              data-source="${original}"
+              alt="${description}"
+            />
+          </a>
+        </div>
+      `;
+    })
+    .join("");
+}
+
+let instance = "";
+
+function onImageClick(event) {
+  event.preventDefault();
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+  instance = basicLightbox.create(
+    `
+    <img src=${event.target.dataset.source}>
+`
+  );
+  instance.show();
+}
+
+// console.log(galleryItems);
